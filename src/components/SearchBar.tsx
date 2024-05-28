20f25df43724ad29dcba6c578ad9fe1a9edfd527
ArkadiNazarian <nazarianarkadi@gmail.com>
2024-05-28 21:51:33 +0330

    add the validation to search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { useFormik } from 'formik'
+import { FormikErrors, useFormik } from 'formik'
 import { CloseIcon } from './ui-icons/close';
+import * as yup from 'yup'
 
 interface ICurrencyNameModel {
   currencyName: string;
@@ -19,25 +20,30 @@ export const SearchBarComponent = () => {
     router.replace(`/coin/${value.currencyName.toLowerCase()}`)
   }
 
+  const validationSchema = yup.object().shape({
+    currencyName: yup.string().required("This field is required"),
+  });
+
   const formik = useFormik({
     initialValues: initialValues,
+    validationSchema: validationSchema,
     onSubmit: actionSearch
   })
 
   const handlerClean = () => {
-    formik.setFieldValue("currencyName", "")
+    formik.resetForm()
   }
 
   return (
     <div className="bg-white w-[32vw] h-[30vh] border rounded-3xl flex justify-center items-center shadow-sm">
       <form onSubmit={formik.handleSubmit} className="text-center text-black flex flex-col gap-[4vh] justify-center items-center">
-        <div className='flex justify-center items-center border border-custom_dark_gray focus-within:border-custom_purple p-[0.9vw] rounded-md'>
+        <div className={`flex justify-center items-center border border-custom_dark_gray focus-within:border-custom_purple p-[0.9vw] rounded-md ${formik.errors.currencyName && "border-red-600"}`}>
           <input
             type="text"
             value={formik.values.currencyName}
             onChange={formik.handleChange}
             name="currencyName"
-            className="w-[22vw] text-sm placeholder:text-custom_extra_dark_gray focus:placeholder:text-custom_medium_gray outline-none "
+            className={`w-[22vw] text-sm placeholder:text-custom_extra_dark_gray focus:placeholder:text-custom_medium_gray outline-none ${formik.errors.currencyName && "placeholder:text-red-600"}`}
             placeholder="Currency name"
           >
           </input>
