462a592a5d7a75eb9170a4c2ad9d54d05fd879c7
ArkadiNazarian <nazarianarkadi@gmail.com>
2024-05-29 08:53:07 +0330

    remove unnecessary div

--- src/app/coin/[coinId]/not-found.tsx
+++ src/app/coin/[coinId]/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="text-center">
-      <h1 className="text-3xl font-bold mb-8">404 page , not found</h1>
+      <p className="text-3xl font-bold mb-8">404 page , not found</p>
     </div>
   )
 }
