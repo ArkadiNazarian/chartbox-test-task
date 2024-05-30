f331ae8a77b88851eea4493c5c924bbf7560d421
ArkadiNazarian <nazarianarkadi@gmail.com>
2024-05-30 15:29:11 +0330

    fix the 404 page

--- src/app/coin/[coinId]/not-found.tsx
+++ src/app/coin/[coinId]/not-found.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import { DangerIcon } from "@/components/ui-icons/danger";
+import { useParams } from "next/navigation";
+
 export default function NotFound() {
+
+  const pathname = useParams();
+
   return (
-    <div className="text-center">
-      <p className="text-3xl font-bold mb-8">404 page , not found</p>
+    <div className="bg-white w-[32vw] border rounded-3xl flex items-center p-[3vw] shadow-sm">
+      <div className="flex items-center gap-[0.5vw]">
+        <DangerIcon />
+        <p className="text-sm font-semibold">There is no result for "{pathname.coinId}"!</p>
+      </div>
     </div>
   )
 }
